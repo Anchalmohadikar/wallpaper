d24982f83e3ef465dcf3662e010ff43e5c198bb2
Parth Aggarwal <frazile.com@gmail.com>
2020-11-03 16:22:00 +0530

    All files arranged & recreated & Bugs Fied

---
+++ lib/Global.dart
@@ -0,0 +1,6 @@
+import 'package:hello/model/photos.dart';
+
+class Global {
+  static List<Photo> photos = new List();
+  static int Index = 0;
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hello/fullpage.dart';
+import 'package:hello/wallpapers.dart';
+
+void main() {
+  runApp(ConfigApp());
+}
+
+class ConfigApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Wallpapers(),
+        'fullpage': (context) => FullPage(),
+      },
+    );
+  }
+}
